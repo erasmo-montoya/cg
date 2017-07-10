45eefe10a5016cae001ac5a34229afd1fa946da3
Rodolfo Galvez <rodolfog@Rodolfos-iMac.local>
2017-07-10 13:35:22 -0500

    init

---
+++ func.js
@@ -0,0 +1,27 @@
+$( document ).ready(function() {
+	//test
+	leer("Eruptions", ["Year","Country","DEATHS","Month","Day"]);
+});
+
+
+function leer(name, att){
+	var data = {
+		tableName: name,
+		attributes: att
+	};
+
+	$.ajax({
+		url: 'https://lypqoj49qj.execute-api.us-east-2.amazonaws.com/dev/disasters/getData',
+		type: 'POST',
+		crossDomain: true,
+		contentType: 'application/json',
+		data: JSON.stringify(data),
+		dataType: 'json',
+		success: function(data) {
+			console.log(data);
+		},
+		error: function(xhr, ajaxOptions, thrownError) {
+			console.log(thrownError);
+		}
+	});
+}
